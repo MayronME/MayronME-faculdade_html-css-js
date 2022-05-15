1942c87c628c27f3db785c4e6c5994a2222fa6d2
Mayron Entreportes <mayron795@gmail.com>
2022-05-15 10:13:33 -0300

    adicionando documentos complementares

--- pratica dia 06.05/js/script.js
+++ pratica dia 06.05/js/script.js
@@ -1,3 +1,5 @@
+document.write("<h1>Estamos executando o script.js</h1>");
+document.write("Esse script está em um arquivo externo e é referenciado no header do HTML");
 console.log("Esse script está no cabeçalho do HTML");
 console.log("Primeiro");
 console.log("Segundo");
@@ -12,11 +14,11 @@ console.log("Segundo");
 
 // Tipagem fraca
 var teste = 1;
-document.write("O valor da variável teste é " + teste + "<hr>");
+document.write("<hr>O valor da variável teste é " + teste + "<hr>");
 teste = 'joao';
 document.write("O valor da variável teste agora é " + teste + "<hr>");
 
-// Variável global
+// Variável global - criada sem necessidade de declaração
 nome = 'joao';
 console.log(nome)
 
@@ -31,6 +33,12 @@ console.log(typeof(v3));
 console.log(typeof NaN); // tipo number
 console.log(typeof +Infinity); // tipo number
 console.log(typeof null); // tipo number
+const number = "1";
+// Operadores de comparação
+console.log(number == 1);
+// true
+console.log(number === 1);
+// false
 
 console.log('ele disse "vtnc"');
 
@@ -42,9 +50,9 @@ var body = document.getElementsByTagName('body');
 console.log(listaUl);
 console.log(body);
 
-body[0].appendChild(listaUl);
+//alert("Vamos criar uma lista aqui!");
 
-document.write("Lista não ordenada criada por JavaScript");
+body[0].appendChild(listaUl);
 
 for(var  i=0;i<lista.length;i++) {
     var liFor = document.createElement('li');
@@ -53,4 +61,29 @@ for(var  i=0;i<lista.length;i++) {
     listaUl.appendChild(liFor);
 }
 
+document.write("A lista não ordenada <b>ACIMA</b> foi criada dinamicamente via JavaScript");
 
+var obj = {
+    nome: "Joao",
+    sobrenome: "Furtado",
+    profissao: "jogador",
+    salario: 120000,
+    pessoaJuridica: true
+};
+
+console.log(obj);
+console.log("Salario: R$" + obj.salario);
+
+var arr = [5, "JP", true, {teste1: 1, teste2: 2}, false];
+console.log(arr);
+console.log(arr[1]);
+console.log(typeof arr);
+
+if(arr[4]) {
+    console.log("Entrou no teste do IF")
+}
+
+// Operadores de comparação ==, ===, !=, >, <, >=, <= 
+if (obj.salario > '100000') {
+    console.log("Salário acima de 100 mil");
+}
--- pratica dia 06.05/js/script2.js
+++ pratica dia 06.05/js/script2.js
@@ -1,1 +1,2 @@
-console.log("Esse script está em um arquivo externo e é referenciado no final do body do HTML")
+document.write("<h1>Estamos executando o script2.js</h1>");
+document.write("Esse script está em um arquivo externo e é referenciado no final do body do HTML");
